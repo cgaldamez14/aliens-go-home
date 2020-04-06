3b1eef4b0da4f6bcaa576d9532123341088e2833
Carlos Galdamez <carlos.galdamez@Carloss-MacBook-Pro.local>
2020-04-05 17:04:45 -0700

    created ground and sky components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import Canvas from "./components/Canvas";
 
-function App(props) {
+function App() {
   return (
     <div className="App">
-      <h1>{props.message}</h1>
+        <Canvas/>
     </div>
   );
 }
 
 App.propTypes = {
     message: PropTypes.string.isRequired
-}
+};
 
 export default App;
