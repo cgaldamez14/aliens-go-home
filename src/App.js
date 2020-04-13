c08c336e46d34648981ad64c0d1dd575b5b1514f
Carlos Galdamez <galdamez.cm@gmail.com>
2020-04-12 17:48:32 -0700

    added movement to cannon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types'
 import Canvas from "./components/Canvas";
+import {getCanvasPosition} from "./utils/formulas";
 
-function App() {
+function App(props) {
+    let canvasMousePosition = undefined;
+
+    useEffect(() => {
+       const interval = setInterval(() => {
+           props.moveObjects(canvasMousePosition);
+       }, 10)
+       return () => clearInterval(interval);
+    },[canvasMousePosition, props]);
+
+    const trackMouse = (event) => {
+      canvasMousePosition = getCanvasPosition(event)
+    };
+
   return (
-    <div className="App">
-        <Canvas/>
-    </div>
+      <Canvas
+        angle={props.angle}
+        trackMouse={trackMouse}
+      />
   );
 }
 
 App.propTypes = {
-    message: PropTypes.string.isRequired
+    angle: PropTypes.number.isRequired,
+    moveObjects: PropTypes.func.isRequired
 };
 
 export default App;
